5b72c8497e655d3dfce5987554483b23c9ca46f3
Emanoel Cariolando <136757338+EmanoelCariolando@users.noreply.github.com>
2025-05-21 21:41:00 -0300

    First Exercise

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,38 @@
+import entities.Comments;
+import entities.Post;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-       
+    public static void main(String[] args) throws ParseException {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+        Comments c1 = new Comments("Wow Awesome!");
+        Comments c2 = new Comments("Welcome!");
+
+        Post p1 = new Post(
+                sdf.parse("21/06/2018 13:05:44"),
+                "My first Post",
+                "give me welcome!",
+                3);
+        p1.addComment(c1);
+        p1.addComment(c2);
+
+        Comments c3 = new Comments("me too!");
+        Comments c4 = new Comments("Just Study!");
+
+        Post p2 = new Post(
+                sdf.parse("22/07/2018 10:02:00"),
+                "I need Money!",
+                "any tips?",
+                9);
+        p2.addComment(c3);
+        p2.addComment(c4);
+
+        System.out.println(p1);
     }
 }
---
+++ src/entities/Comments.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class Comments {
+    private String comment;
+
+    public Comments(String comment) {
+        this.comment = comment;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    @Override
+    public String toString() {
+        return  comment;
+    }
+}
