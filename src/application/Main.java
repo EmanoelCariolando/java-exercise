9b2303094d9567d9a52a29bde620c152a2483b49
Emanoel Cariolando <136757338+EmanoelCariolando@users.noreply.github.com>
2025-08-27 21:21:18 -0300

    Basics Streams

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,36 +4,24 @@ import application.util.Verification;
 
 import java.text.ParseException;
 import java.util.*;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws ParseException {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        List<Product> list = new ArrayList<>();
 
-        list.add(new Product("TV", 50.00));
-        list.add(new Product("PC", 30.00));
-        list.add(new Product("Play", 40.00));
-        list.add(new Product("Tablet", 60.00));
-        list.add(new Product("Console", 100.00));
-        list.add(new Product("Control", 80.00));
+        List<Integer> list1 = Arrays.asList(1,2,3,5,10);
 
-        System.out.print("Type one letter of your product for sum: ");
-        char letter = sc.next().charAt(0);
+        Stream<Integer> st1 = list1.stream();
+        System.out.println(Arrays.toString(st1.toArray()));
 
-        Verification veri = new Verification();
+        Stream<String> st2 =  Stream.of("Maria","João", "Bob");
+        System.out.println(Arrays.toString(st2.toArray()));
 
-        double sum = veri.filtered(list, p -> p.getName().charAt(0) == letter );
-
-        System.out.println("All sums: " + sum);
-
-
-
-
-
-
-
-
+        Stream<Integer> st3 = Stream.iterate(0,x -> x + 2);
+        System.out.println(Arrays.toString(st3.limit(10).toArray()));
+        
 
 
     }
